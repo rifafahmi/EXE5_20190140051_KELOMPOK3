9fed81513c8f297b96406c597dc21ff016000db8
Rifa <rifafahim21.com@gmial.com>
2022-12-22 16:42:09 +0700

    insert

--- EXE5_20190140051_KELOMPOK3/Program.cs
+++ EXE5_20190140051_KELOMPOK3/Program.cs
@@ -18,6 +18,29 @@ namespace EXE5_20190140051_KELOMPOK3
             rifa = null;
             fahmi = null;
         }
+        public void insert()
+        {
+            string nm;
+            node newNode = new node();
+            Console.WriteLine("masukan element: ");
+            nm = Console.ReadLine();
+            newNode.name = nm;
+            newNode.next = null;
+            if (rifa = null)
+            {
+                rifa = newNode;
+                fahmi = newNode;
+                return;
+
+            }
+            rifa.next = newNode;
+            rifa = newNode;
+
+
+
+
+            
+        }
     }
     
 }
